c254cc61caff7ee00ed7909edc374b9c38dc7a7f
Kohsuke Kawaguchi <kk@kohsuke.org>
2006-03-24 06:53:18 +0000

    added code to avoid duplicates.

--- src/org/kohsuke/parseipr/ClasspathBuilder.java
+++ src/org/kohsuke/parseipr/ClasspathBuilder.java
@@ -1,6 +1,8 @@
 package org.kohsuke.parseipr;
 
 import java.io.File;
+import java.util.Set;
+import java.util.HashSet;
 
 /**
  * Builds a CLASSPATH string from {@link File}s.
@@ -14,29 +16,37 @@ public class ClasspathBuilder {
      * The default value is platform-dependent.
      */
     private String separator = File.pathSeparator;
-    
+
     protected final StringBuffer buf = new StringBuffer();
-    
+
+    /**
+     * Used for checking duplicates.
+     */
+    private final Set files = new HashSet();
+
     /**
      * Overrides the platform-default separator string.
      */
     public void setSeparator( String sep ) {
         this.separator = sep;
     }
-    
+
     public void reset() {
         buf.setLength(0);
     }
-    
+
     /**
      * Adds a new entry
      */
     public void add( File f ) {
+        if(!files.add(f))
+            return; // already added
+
         if( buf.length()!=0 )
             buf.append(separator);
         buf.append(f.toString());
     }
-    
+
     /**
      * Returns the string formatted for the CLASSPATH variable.
      */
